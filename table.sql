3ab27b3057e5d1028e487ce8eea6df4eaeeca531
akhilrgeorge007 <akhilrgeorge007@gmail.com>
2022-12-10 21:09:47 +0530

    sqlupdated

--- table.sql
+++ table.sql
@@ -1,3 +1,4 @@
+
 CREATE TABLE Tenant(
 Id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 Name varchar(200),
@@ -11,3 +12,27 @@ Name varchar(200),
 Email varchar(200),
 Password varchar(200)
 );
+
+CREATE TABLE Property(
+Id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+Name varchar(200),
+Location varchar(200),
+Type varchar(10),
+Description varchar(300),
+Max_occupant int,
+Current_occupant int,
+Owner_id int, 
+FOREIGN KEY(Owner_id) REFERENCES Owner(Id)
+);
+
+
+CREATE TABLE PropertyCost(
+Property_id int,
+Gas int,
+Water int,
+Electricity int,
+Rent int,
+FOREIGN KEY(Property_id) REFERENCES Property(Id)
+);
+
+
