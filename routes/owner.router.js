3997b31e67dd4cdcbc2b7e8609ffc9ebaa03479f
akhilrgeorge007 <akhilrgeorge007@gmail.com>
2022-12-11 10:37:06 +0530

    Addproperty and cost done

--- routes/owner.router.js
+++ routes/owner.router.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const {getOwnerController, registerOwnerController,loginOwnerController}  = require('../controllers/owner.controller');
+const {
+    getOwnerController, 
+    registerOwnerController,
+    loginOwnerController,
+    addProperty,
+    updatePropertyCost
+}  = require('../controllers/owner.controller');
 
 router.get('/',getOwnerController);
 router.post('/register',registerOwnerController);
 router.post('/login',loginOwnerController)
+
+router.post('/:id/listproperty',addProperty), // id is owner id
+
+router.put('/:id/updatecost',updatePropertyCost), //id: is property_id
+
 
 
 module.exports = router;
